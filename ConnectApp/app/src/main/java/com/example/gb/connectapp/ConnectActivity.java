b960c0577887c14abbc3409b6dbb2ee71527a01a
Gamab <gabi.mabs@gmail.com>
2015-03-31 17:20:37 +0200

    [Gab] Petit test dans le connect.

--- ConnectApp/app/src/main/java/com/example/gb/connectapp/ConnectActivity.java
+++ ConnectApp/app/src/main/java/com/example/gb/connectapp/ConnectActivity.java
@@ -46,8 +46,10 @@ public class ConnectActivity extends ActionBarActivity {
                 sh.deleteEverything();
 
                 // ADD Networks AND RETURN THEIR IDS
-                long net2 = Ajout_Reseau_BDD("GabMab", "12345_wifi");
-                long net1 = Ajout_Reseau_BDD("Yann Mb", "12345_wifi");
+                //long net2 = Ajout_Reseau_BDD("GabMab", "12345_wifi");
+                //long net1 = Ajout_Reseau_BDD("Yann Mb", "12345_wifi");
+                long net1 = sh.addNetwork("Yann Mb", "36:5A:05:A4:DE:67", "pass12345__");
+                long net2 = sh.addNetwork("gifi", "F8:E0:79:3D:33:3C", "pass12345__");
 
                 // ADD Qos AND RETURN THEIR IDS
                 long qos1 = Evaluation_Reseau(net1, "5", "16:00:00");
@@ -56,12 +58,23 @@ public class ConnectActivity extends ActionBarActivity {
                 //String ssid=sh.getNetworkByMark();
                 //Connect(ssid, sh.getPresharKeyBySSID(ssid));
                 //NetworkDesc nd = sh.getNetworkByNote();
-                //ArrayList<String> wifis = new ArrayList<String>();
-                //wifis.add("JCWifi");
-                //wifis.add("Yann Mb");
-                //NetworkDesc nd = sh.getNetworkByNoteFrom(wifis);
-                //Log.d(TAG,"Best network is : SSID : " + nd.getmName() + " PKEY : " + nd.getmPass());
+                ArrayList<NetworkDesc> wifis = new ArrayList<>();
+                wifis.add(new NetworkDesc("Yann Mb", "36:5A:05:A4:DE:67",""));
+                wifis.add(new NetworkDesc("gifi", "F8:E0:79:3D:33:3C",""));
+                wifis.add(new NetworkDesc("JCwifi", "36:5A:05:C5:DF:89",""));
 
+                NetworkDesc nd = sh.getNetworkByNoteFrom(wifis);
+                if (nd != null) {
+                    if (nd.getmName() != null) {
+                        Log.d(TAG, "Best network is : SSID : " + nd.getmName() + " PKEY : " + nd.getmPass());
+                    }
+                    else {
+                        Log.d(TAG, "Could not found best WIFI.");
+                    }
+                }
+                else {
+                    Log.d(TAG, "Could not found best WIFI.");
+                }
                 //Connect(nd.getmName(),nd.getmPass());
                 sh.printDatabase();
 

43d3f6cda751835322cb693550f5eb7b0c41c0de
iamyann <yann.mb94@gmail.com>
2015-05-05 16:30:04 +0200

    [Yann] Application parfaite pour noter ton WIFI

---
+++ Wifi_Notation/app/src/main/java/introduction/cours/androidstudio/wifi_notation/NetworkTable.java
@@ -0,0 +1,17 @@
+package introduction.cours.androidstudio.wifi_notation;
+
+/**
+ * Created by Yann MB on 04/05/2015.
+ */
+public class NetworkTable {
+    // EACH Network HAS UNIQUE ID
+    public static final String ID = "_id";
+    // SSID OF THE Network
+    public static final String SSID = "SSID";
+    // BSSID OF THE Network
+    public static final String BSSID = "BSSID";
+    // Key OF Network
+    public static final String PresharedKey = "PresharedKey";
+    // NAME OF THE TABLE
+    public static final String TABLE_NAME = "Network";
+}
